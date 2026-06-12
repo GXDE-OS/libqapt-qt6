24cc38d872db7886eda5b98c4a7a4560a28df7e6
gfdgd_xi <3025613752@qq.com>
2026-06-12 18:52:36 +0800

    修复 pkg-config 调用时无法找到正常 include 文件夹的问题
    Signed-off-by: gfdgd_xi <3025613752@qq.com>

--- libqapt-qt6.pc.cmake
+++ libqapt-qt6.pc.cmake
@@ -1,7 +1,7 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=@CMAKE_INSTALL_PREFIX@
 libdir=@CMAKE_INSTALL_PREFIX@/lib@LIB_SUFFIX@
-includedir=@CMAKE_INSTALL_PREFIX@/include/libqapt-qt6
+includedir=@CMAKE_INSTALL_PREFIX@/include/qapt-qt6
 
 Name: libqapt-qt6
 Description: Qt wrapper around libapt-pkg
